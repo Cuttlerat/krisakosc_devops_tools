bf02888e1f4eb18b29b5db2f3205002c66551606
cuttlerat <avkioller@gmail.com>
2018-02-05 11:04:54 +0300

    Updated fastpost

---
+++ roles/fastpost/files/local_settings.py
@@ -0,0 +1,21 @@
+from .settings import INSTALLED_APPS, MIDDLEWARE_CLASSES
+
+SECRET_KEY = 'Your secret key'
+DEBUG = True # False if your want to use fastpost in production
+ALLOWED_HOSTS = ['*'] # for docker-compose
+
+STATIC_ROOT = "static/" # For nginx in docker
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'LoshadkaSkobkaBatareyka',
+        'HOST': 'fastpost_postgres',
+        'PORT': '5432',
+    }}
+
+# if you want to use debug_toolbar
+#INSTALLED_APPS.append('debug_toolbar')
+#MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
